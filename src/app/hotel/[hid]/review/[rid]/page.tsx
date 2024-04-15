5fb059e00fac5890ef609153e3e3e361224ae4ec
Patipon Sanor <6633134721@student.chula.ac.th>
2024-04-15 23:23:00 +0700

    Use ReplyArea in reply page

--- src/app/hotel/[hid]/review/[rid]/page.tsx
+++ src/app/hotel/[hid]/review/[rid]/page.tsx
@@ -1,5 +1,5 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
-import ReplyForm from "@/components/ReplyForm";
+import ReplyArea from "@/components/ReplyArea";
 import getHotel from "@/libs/getHotel";
 import getReview from "@/libs/getReview";
 import getUserProfile from "@/libs/getUserProfile";
@@ -9,12 +9,12 @@ import Image from "next/image";
 export default async function ReviewPage({ params }: { params: { hid: string, rid: string } }) {
     const session = await getServerSession(authOptions);
 
-    let userProfile = null;
-    if (session) userProfile = await getUserProfile(session?.user.token);
+    let userProfileRes = null;
+    if (session) userProfileRes = await getUserProfile(session?.user.token);
 
     const reviewRes = await getReview(params.rid);
     const review = reviewRes.data;
-    console.log(reviewRes);
+    // console.log(reviewRes);
 
     const hotelRes = await getHotel(review.hotel);
     const hotel = hotelRes.data;
@@ -41,40 +41,34 @@ export default async function ReviewPage({ params }: { params: { hid: string, ri
     return (
         <main>
             {
-                userProfile ?
-                    reviewRes.success ?
-                        <>
-                            <div className="flex justify-center w-full">
-                                <div className="w-2/3 my-4 h-fit justify-center bg-white rounded-lg shadow-xl">
-                                    <div className="flex flex-row mx-8 py-8">
-                                        <div className="w-fit mr-4">
-                                            <h1 className="font-bold text-2xl mb-2 text-black">
-                                                {review.user.name}
-                                            </h1>
-                                            <span className="flex gap-2 text-nowrap mb-3">
-                                                <Image
-                                                    src="/icon/bedicon.png"
-                                                    alt="bed icon"
-                                                    fill
-                                                    className="!relative !h-[1.5rem] !w-fit object-contain"
-                                                />
-                                                Hotel : {hotel.name}
-                                            </span>
-                                            <div className="text-sm text-[#78819a] mt-10">Reviewed at {new Date(review.createdAt).toLocaleDateString()}</div>
-                                        </div>
-                                        <div className="w-[80%] px-20"><p className="break-words my-2">{review.message}</p></div>
-                                        <div className="w-fit text-right text-[#7881a9] text-2xl text-nowrap">4.54 ⭐</div>
+                reviewRes.success ?
+                    <>
+                        <div className="flex justify-center w-full">
+                            <div className="w-2/3 my-4 h-fit justify-center bg-white rounded-lg shadow-xl">
+                                <div className="flex flex-row mx-8 py-8">
+                                    <div className="w-fit mr-4">
+                                        <h1 className="font-bold text-2xl mb-2 text-black">
+                                            {review.user.name}
+                                        </h1>
+                                        <span className="flex gap-2 text-nowrap mb-3">
+                                            <Image
+                                                src="/icon/bedicon.png"
+                                                alt="bed icon"
+                                                fill
+                                                className="!relative !h-[1.5rem] !w-fit object-contain"
+                                            />
+                                            Hotel : {hotel.name}
+                                        </span>
+                                        <div className="text-sm text-[#78819a] mt-10">Reviewed at {new Date(review.createdAt).toLocaleDateString()}</div>
                                     </div>
+                                    <div className="w-[80%] px-20"><p className="break-words my-2">{review.message}</p></div>
+                                    <div className="w-fit text-right text-[#7881a9] text-2xl text-nowrap">4.54 ⭐</div>
                                 </div>
                             </div>
-                            <div className="flex justify-center w-full">
-                                <div className="flex flex-row object-center w-2/5">
-                                    <ReplyForm userID={review.user._id} rid={params.rid} />
-                                </div>
-                            </div>
-                        </>
-                        : <>Review with id {params.rid} not found</>
-                    : <>Not authorized to access this route</>
+                        </div>
+                        <ReplyArea replyData={review.replys} review={review} rid={params.rid} role={userProfileRes.data.role} />
+                    </>
+                    : <>Review with id {params.rid} not found</>
             }
         </main >
     );
