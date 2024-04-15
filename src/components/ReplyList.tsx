866a2fb092249b7b54d778c4e26c1790cd0662bd
Patipon Sanor <6633134721@student.chula.ac.th>
2024-04-15 23:20:32 +0700

    Create ReplyList component

---
+++ src/components/ReplyList.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+export default function ReplyList({ replyData }: { replyData: {}[] }) {
+  console.log(replyData);
+  return (
+    replyData ?
+      Array.from(replyData).map((reply: any) =>
+        <div key={reply._id} className="my-1 bg-white rounded-lg shadow-xl">
+          <div className="py-4 px-4">
+            <p className="break-words">
+              {reply.message}
+            </p>
+            <div className="text-sm text-[#78819a] mt-10">Reviewed at {new Date(reply.createdAt).toLocaleDateString()}</div>
+          </div>
+        </div>
+      )
+      : <></>
+  );
+}
