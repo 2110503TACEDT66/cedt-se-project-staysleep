ddfc7ab31859599ebc4d36f8163a7533c275cb76
Pittayaice <pittayakhunpitak14@gmail.com>
2024-04-22 22:09:54 +0700

    Add rating and Hotel item

--- frontend/src/components/HotelCatalog.tsx
+++ frontend/src/components/HotelCatalog.tsx
@@ -2,14 +2,15 @@
 import React, { useState, useEffect } from 'react';
 import Card from "./Card";
 import Link from "next/link";
-import InputGroup from 'react-bootstrap/InputGroup';
-import Form from 'react-bootstrap/Form';
 import { FaSearch } from "react-icons/fa";
+import { Rating } from '@mui/material';
+import { hotelItem, hotelJson } from '@/interface';
 
 export default function HotelCatalog() {
     const [search, setSearch] = useState('');
+    const [rating, setrating] = useState(0);
     const [hotels, setHotels] = useState([]);
-
+    //console.log(rating);
     useEffect(() => {fetchData(search)}, [search]);
 
     const fetchData = (value) => {
@@ -25,7 +26,7 @@ export default function HotelCatalog() {
 
     return (
         <>  
-            <div className="text-black  max-w-lg mx-auto p-4 bg-white rounded-lg shadow-md flex items-center">
+            <div className="text-black  max-w-lg mx-auto p-4 bg-white rounded-lg shadow-md flex flex-column items-center">
                 <FaSearch className=" mr-2" />
                 <input
                     className="w-full focus:outline-none"
@@ -34,15 +35,20 @@ export default function HotelCatalog() {
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
                 />
+                <Rating
+                    name="simple-controlled"
+                    value={rating}
+                    onChange={(e) => setrating(e.target.value)}
+                />
             </div>
 
-            <div style={{ margin: "20px", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "space-around", alignContent: "space-around", color: "black" }}>
+            <div style={{ margin: "20px", display: "flex", flexDirection: "column", flexWrap: "wrap", justifyContent: "space-around", alignContent: "space-around", color: "black" }}>
                 {hotels
-                    .filter((hotelItem) => {
+                    .filter((hotelItem: hotelItem) => {
                         const lowercaseSearch = search.toLowerCase();
                         return lowercaseSearch === '' ? hotelItem : hotelItem.name.toLowerCase().includes(lowercaseSearch);
                     })
-                    .map((hotelItem) => (
+                    .map((hotelItem: hotelItem) => (
                         <Link href={`/hotel/${hotelItem.id}`} className="w-[100%] sm:w-[30%] lg:w-[25%] p-2 sm:p-4 lg:p-8 " key={hotelItem.id}>
                         <Card review={hotelItem.reviews} hotelName={hotelItem.name} imgSrc={hotelItem.picture} />
                         </Link>
