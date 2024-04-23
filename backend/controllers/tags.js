1d1121b010bfec9f1ff8256c42f2b30cb5e246c7
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-04-23 13:03:01 +0700

    move merge func to /utils

--- backend/controllers/tags.js
+++ backend/controllers/tags.js
@@ -1,11 +1,14 @@
+const Tags = require('../models/Tags');
+const { merge } = require('../utils/utils');
+
 //@desc Get all tags
 //@route GET /api/v1/hotels/tags
 //@access Public
 exports.getTags = async (req, res, next) => {
-    try{
+    try {
         const tags = await Tags.findOne({});
         res.status(200).json({ success: true, data: tags });
-    }catch(err){
+    } catch (err) {
         res.status(400).json({ success: false });
     }
 }
@@ -17,15 +20,7 @@ exports.createTags = async (req, res, next) => {
     try {
         const tags = await Tags.findOne({});
         if (tags) {
-            // merge tags
-            const merge = (a, b, predicate = (a, b) => a === b) => {
-                const c = [...a]; // copy to avoid side effects
-                // add all items from B to copy C if they're not already present
-                b.forEach((bItem) => (c.some((cItem) => predicate(bItem, cItem)) ? null : c.push(bItem)))
-                return c;
-            }
             tags.tags = merge(tags.tags, req.body.tags);
-            
             const _tags = await Tags.findOneAndUpdate({}, tags);
             res.status(201).json({
                 success: true,
