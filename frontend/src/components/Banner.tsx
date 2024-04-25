414088ede406cbbf778bb8faff4b6531977616ec
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-04-25 16:38:33 +0700

    update banner (a bit)

--- frontend/src/components/Banner.tsx
+++ frontend/src/components/Banner.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 import { useEffect, useRef, useState } from "react";
 import { motion, useAnimationControls } from "framer-motion"
 import { HotelMock } from "@/interface";
-import { FaAngleRight } from "react-icons/fa";
+import { FaAngleRight, FaGithubSquare } from "react-icons/fa";
 import Link from "next/link";
 
 export default function Banner() {
@@ -42,7 +42,7 @@ export default function Banner() {
     clearTimeOut();
     timeOutRef.current = setTimeout(() => {
       setIndex((index + 1) % hotels.length);
-    }, 3500);
+    }, 4000);
     controller.set({ opacity: 0, x: -100 });
     controller.start({ opacity: 1, x: 0 });
 
@@ -65,7 +65,15 @@ export default function Banner() {
       className="h-screen w-screen rel pl-[10rem] flex justify-start items-center"
       onClick={handleBannerClicked}
     >
-      <div className="h-[40rem] w-[20rem]">
+      <div className="h-[40rem] w-[20rem] relative">
+        <Link href="https://github.com/2110503TACEDT66/cedt-se-project-staysleep">
+          <FaGithubSquare className="fixed z-10 m-2 bottom-0 left-0 h-10 w-10 fill-primary" />
+        </Link>
+        <div className="flex items-center text-nowrap m-2 absolute z-20 -rotate-90 origin-top-left top-[64%] -left-[10rem] text-primaryWhite">
+          <div className="absolute w-[7rem] left-full rounded-md border-b-2"></div>
+          🥐 CEDT-STAYSLEEP 🥐
+          <div className="absolute w-[7rem] right-full rounded-md border-b-2"></div>
+        </div>
         <Image src={hotels[index].roomImage} alt="cover" fill priority style={{ objectFit: "cover" }} className="!relative rounded-lg z-10" quality={10} />
       </div>
       <div>
@@ -84,8 +92,8 @@ export default function Banner() {
       <Image src={hotels[index].image} alt="cover" fill priority className="object-cover -z-10 ml-[20rem]" quality={10} />
       <Link
         href="/hotel"
-        className="text-primaryWhite border-[2px] border-primary bg-transparent hover:bg-primary hover:text-black
-        py-2 px-10 mx-8 my-12 z-30 absolute bottom-0 right-0 hover:border-transparent rounded-3xl"
+        className="text-primary border-[2px] border-transparent bg-secondary hover:bg-primary hover:text-secondary
+        py-2 px-10 mx-8 my-12 z-30 absolute bottom-0 right-0 hover:border-secondary rounded-3xl"
       >
         View Hotel
         <FaAngleRight className="inline ml-2" />
