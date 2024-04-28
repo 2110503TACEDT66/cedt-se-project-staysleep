ce71c0ab409c55768bf76c5c5bb8385c98564bd4
Itttikorn <ittikornsd@gmail.com>
2024-04-28 23:53:56 +0700

    change swagger api name

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const swaggerOptions = {
   swaggerDefinition: {
     openapi: '3.0.0',
     info: {
-      title: 'Library API',
+      title: 'Hotel Reservation API',
       version: '1.0.0',
       description: 'An Express Hotel booking API'
     },
