29f87f828549b6433c8ce72d69f4477cff0df9d4
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-04-29 21:46:43 +0700

    Impl ParallaxBanner and add to hotel/ page

--- frontend/src/app/hotel/page.tsx
+++ frontend/src/app/hotel/page.tsx
@@ -1,27 +1,18 @@
-import getHotels from "@/libs/getHotels";
+"use client";
 import Hotelcatalog from "@/components/HotelCatalog";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-import getUserProfile from "@/libs/getUserProfile";
 import { redirect } from "next/navigation";
+import ParallaxBanner from "@/components/ParallaxBanner";
+import { useSession } from "next-auth/react";
 
-export default async function Hotel() {
-
-    const hotels = await getHotels()
-    console.log(hotels)
-    var userRole = "";
-
-    const session = await getServerSession(authOptions);
-    if (!session || !session?.user) return redirect("/api/auth/signin");
-    if (session && session?.user){
-        const user = await getUserProfile(session?.user?.token);
-        userRole = user.data.role;
-    }
+export default function Hotel() {
+    const session = useSession();
+    if (!session || !session?.data?.user.role) return redirect("/api/auth/signin");
 
     return (
-        <main className="text-center p-5 pt-[7rem]">
+        <main className="text-center p-5 pt-[20rem] bg-black z-0">
+            <ParallaxBanner />
             <h1 className="text-xl font-medium text-primaryWhite">Choose Your Accommodation</h1>
-            <Hotelcatalog userRole = {userRole}/>
+            <Hotelcatalog userRole={session.data.user.role} />
         </main>
     )
 }
