90be9721aa047876e4634f78c2816642d74e5b39
Itttikorn <ittikornsd@gmail.com>
2024-04-29 18:08:14 +0700

    added basic jest for get tags

---
+++ backend/__test__/tags.test.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const request = require("supertest");
+const app = require("../server.js");
+
+require("dotenv").config();
+  
+  describe("GET /api/v1/tags", () => {
+    it("should return all tags", async () => {
+      const res = await request(app).get("/api/v1/hotels/tags");
+      expect(res.statusCode).toBe(200);
+      expect(res.body.data.tags.length).toBeGreaterThan(0);
+    });
+  });
