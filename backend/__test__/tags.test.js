06e7265c0c6c3fb410c88c1a85be1d4a827af240
Itttikorn <ittikornsd@gmail.com>
2024-04-29 18:20:34 +0700

    fix route for testing

--- backend/__test__/tags.test.js
+++ backend/__test__/tags.test.js
@@ -4,7 +4,7 @@ const app = require("../server.js");
 
 require("dotenv").config();
   
-  describe("GET /api/v1/tags", () => {
+  describe("GET /api/v1/hotels/tags", () => {
     it("should return all tags", async () => {
       const res = await request(app).get("/api/v1/hotels/tags");
       expect(res.statusCode).toBe(200);
