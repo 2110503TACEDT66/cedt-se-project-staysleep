af4cb2b07db904354f7892481c33447b564d479b
NattaneeS <144552623+NattaneeS@users.noreply.github.com>
2024-04-30 08:56:45 +0700

    add test

---
+++ backend/__test__/tags2.test.js
@@ -0,0 +1,21 @@
+import {getTags,createTags,deleteTags} from '../controllers/tags';
+
+jest.mock('../models/Tags', ()=> ({
+    findOne : jest.fn().mockReturnValue(null)
+    ,create : jest.fn().mockReturnValue(1),
+}));
+
+
+describe("POST /api/v1/hotels/tags", () => {
+    it('should return null', async () => {
+        var req = {body : "test"};
+        var res = null;
+        try{
+            res = await createTags(req,new Response)
+        }
+        catch(err){
+
+        }
+        expect(res).toEqual(null)
+    });
+  });
