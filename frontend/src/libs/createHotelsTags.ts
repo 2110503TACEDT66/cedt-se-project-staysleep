23740741cf97e9b9e97282f365601bd068e7c8c0
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-04-30 15:38:54 +0700

    forgot to wite req body

--- frontend/src/libs/createHotelsTags.ts
+++ frontend/src/libs/createHotelsTags.ts
@@ -1,7 +1,17 @@
 "use server";
 const createHotelsTags = async (token: string, tag: string) => {
     const response = await fetch(
-        `${process.env.BACKEND_URL}/api/v1/hotels/tags`
+        `${process.env.BACKEND_URL}/api/v1/hotels/tags`,
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                tags: [tag],
+            }),
+        }
     );
     const data = await response.json();
     return data;
--- frontend/src/libs/deleteHotelsTags.ts
+++ frontend/src/libs/deleteHotelsTags.ts
@@ -1,7 +1,17 @@
 "use server";
 const deleteHotelsTags = async (token: string, tag: string) => {
     const response = await fetch(
-        `${process.env.BACKEND_URL}/api/v1/hotels/tags`
+        `${process.env.BACKEND_URL}/api/v1/hotels/tags`,
+        {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                authorization: `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+                tag: [tag],
+            }),
+        }
     );
     const data = await response.json();
     return data;
