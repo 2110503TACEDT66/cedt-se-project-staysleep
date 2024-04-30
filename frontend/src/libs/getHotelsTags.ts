fbc5c8c9da1b1a539e300aa5b037398e981703ef
Itttikorn <ittikornsd@gmail.com>
2024-04-30 16:38:22 +0700

    add no cache to get tags

--- frontend/src/libs/getHotelsTags.ts
+++ frontend/src/libs/getHotelsTags.ts
@@ -1,7 +1,7 @@
 "use server";
 const getHotelsTags = async () => {
     const response = await fetch(
-        `${process.env.BACKEND_URL}/api/v1/hotels/tags`
+        `${process.env.BACKEND_URL}/api/v1/hotels/tags`,{cache: 'no-store'}
     );
     const data = await response.json();
     return data;
